99f7d3314c8168cea437777247ad41e7384be7ae
david_smith <david_smith@sweetwater.com>
2025-01-27 07:41:45 -0500

    main Updated `src/SrcCommand.php`.

--- src/SrcCommand.php
+++ src/SrcCommand.php
@@ -8,11 +8,12 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
-    name: ':package_slug:src',
+    name: SrcCommand::signature,
     description: 'Project source link'
 )]
 class SrcCommand extends Command
 {
+    public const signature = ':package_namespace:src';
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
