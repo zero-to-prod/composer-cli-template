330759260589f8bc22a7dbdfa3c240722f905d6f
David Smith <dasmith@zero-to-prod.com>
2025-02-18 06:15:03 -0500

    main Updated `src/Package.php`.

--- src/Package.php
+++ src/Package.php
@@ -5,8 +5,16 @@ namespace Zerotoprod\:namespace;
 use Symfony\Component\Console\Application;
 use Zerotoprod\:namespace\Src\SrcCommand;
 
+/**
+ * :description
+ *
+ * @link https://github.com/zero-to-prod/:slug
+ */
 class :namespace
 {
+    /**
+     * @link https://github.com/zero-to-prod/:slug
+     */
     public static function register(Application $Application): void
     {
         $Application->add(new SrcCommand());
